ceef7005f79f008c596b14217308db08bfd1e0f0
Dmitrii <dmitriy.site1@gmail.com>
2026-06-08 18:22:16 -0300

    feat: mongo index setup

---
+++ app/db/indexes.py
@@ -0,0 +1,25 @@
+from pymongo import ASCENDING
+from pymongo import DESCENDING
+from pymongo.asynchronous.database import AsyncDatabase
+
+
+async def ensure_indexes(db: AsyncDatabase) -> None:
+    await db.users.create_index([("email", ASCENDING)], unique=True)
+
+    await db.forms.create_index([("owner_id", ASCENDING), ("status", ASCENDING)])
+
+    await db.form_versions.create_index(
+        [("form_id", ASCENDING), ("version", ASCENDING)], unique=True
+    )
+
+    await db.responses.create_index(
+        [("form_id", ASCENDING), ("submitted_at", DESCENDING), ("_id", DESCENDING)]
+    )
+    await db.responses.create_index(
+        [("form_id", ASCENDING), ("answers.key", ASCENDING)],
+    )
+
+    await db.draft_responses.create_index(
+        [("expires_at", ASCENDING)],
+        expireAfterSeconds=0,
+    )
